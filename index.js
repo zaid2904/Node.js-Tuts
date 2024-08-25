1e28fcb50180d543592961f77a62844617509f36
Zaid <104847431+zaid2904@users.noreply.github.com>
2024-08-25 11:52:48 +0530

    Add files via upload
    Async

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+// 1 folder created
+// fs.mkdir('chfld', (err)=>{
+//     console.log("floder created");
+//     console.log(err);
+// })
+// 2 File Created
+// fs.writeFile('chfld/bio.txt', 'my name is zaid👾', (err)=>{
+//     console.log("file created");
+//     console.log(err);
+// })
+// 3 Data added
+// fs.appendFile("chfld/bio.txt","data added 👌", (err)=>{
+//     console.log("added");
+//     console.log(err);
+// })
+
+// 4 read
+// fs.readFile("chfld/bio.txt", "utf-8", (err, data)=>{
+//     console.log(data);
+    
+// })
+
+// 5 rename
+// fs.rename("chfld/bio.txt", "chfld/newName.txt", (err)=>{
+//     console.log(err);
+//     console.log("suxxex");
+    
+// }).
+// delete
+// fs.rmdir("chfld", (err)=>{
+//     console.log("delete");
+//     console.log(err);
+    
+// })
